e39cc2f4fc7622b9922f9205f23b05959f87154f
Alexandre <alexandre.monteiro.bec@gmail.com>
2020-03-29 20:24:42 -0300

    feat: initial setting

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'classes',
+    'import-helpers'
+  ],
+  rules: {
+    indent: ['error', 'tab'],
+    'no-tabs': 'off',
+    'linebreak-style': 'off',    
+    'comma-spacing': 2,
+    'no-multiple-empty-lines': [2, { 'max': 1 }],
+    'no-trailing-spaces': 2,
+    'classes/name': [2, 'class', 'method'],
+    'class-methods-use-this': 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+          newlinesBetween: 'always',
+          groups: [
+            '/^react/',
+            'module',
+            ['parent', 'sibling', 'index'],
+          ],
+          alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+  },
+};
---
+++ packages/backend/src/backend.ts
@@ -0,0 +1,7 @@
+class Teste {
+	teste() {
+		return 'teste ';
+	}
+}
+
+export default Teste;
