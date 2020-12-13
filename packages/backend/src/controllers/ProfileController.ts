47b58c057a2a2569a37c2063f034a5f70d3f7beb
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2020-12-13 16:18:32 -0300

    refactor: removing unnecessary condition

--- packages/backend/src/controllers/ProfileController.ts
+++ packages/backend/src/controllers/ProfileController.ts
@@ -6,12 +6,8 @@ class ProfileController {
 	async index(request: Request, response: Response): Promise<any> {
 		const ong_id = request.headers.authorization;
 
-		if (ong_id) {
-			const incidents = await connection('incidents').where('ong_id', ong_id).select('*');
-			return response.json(incidents);
-		}
-
-		return response.status(401).json({ error: 'Operation not permitted.' });
+		const incidents = await connection('incidents').where('ong_id', ong_id || 0).select('*');
+		return response.json(incidents);
 	}
 }
 
